979548f6f4630e1edd9d45401e5749fd4f484d85
andrés vallejo labrada <elfos_11@hotmail.com>
2024-11-12 20:26:28 -0500

    Se adiciona el formulario de login y register. Se adicionan los permisos a las diferentes vistas

--- equipos/api/permissions.py
+++ equipos/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import BasePermission
 
 class IsAdminReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method == 'GET' or request.method == 'POST' or request.method == 'PUT' or request.method == 'DELETE' :
+        if request.method == 'GET':
             return True
         else:
             return request.user.is_staff
